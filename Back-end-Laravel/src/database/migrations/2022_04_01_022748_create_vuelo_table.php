a51ee5846531965550ceee82608fc2ac622771b5
IAservitech Soluciones Inteligentes <96543528+iaservitech@users.noreply.github.com>
2022-03-31 21:58:03 -0500

    [Add]: Contenedor en Docker de Back-end

---
+++ Back-end-Laravel/src/database/migrations/2022_04_01_022748_create_vuelo_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vuelo', function (Blueprint $table) {
+            $table->id();
+            $table->string('origen');
+            $table->string('destino');
+            $table->string('duracion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vuelo');
+    }
+};
