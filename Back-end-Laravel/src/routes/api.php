615df73264c4372bbf001067dc8c719e5f7c9195
IAservitech Soluciones Inteligentes <96543528+iaservitech@users.noreply.github.com>
2022-04-01 02:24:11 -0500

    [Add]:  Controlador y endpoints de Vuelo

--- Back-end-Laravel/src/routes/api.php
+++ Back-end-Laravel/src/routes/api.php
@@ -19,4 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/persona','App\Http\Controllers\PersonaController@index'); //Todos los registros
-Route::get('/persona/{id}','App\Http\Controllers\PersonaController@show');
+Route::get('/persona/{id}','App\Http\Controllers\PersonaController@show'); //Por id
+
+Route::get('/vuelo','App\Http\Controllers\VueloController@index'); //Todos los registros
+Route::get('/vuelo/{id}','App\Http\Controllers\VueloController@show');
